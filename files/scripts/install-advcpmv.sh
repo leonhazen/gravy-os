07d9e57146ae622f3ec9506c36353819e2b82fa8
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2025-05-27 23:30:01 +1000

    lets try again

--- files/scripts/install-advcpmv.sh
+++ files/scripts/install-advcpmv.sh
@@ -2,6 +2,9 @@
 
 set -oue pipefail
 
+mkdir -p /usr/local/bin
+
 curl https://raw.githubusercontent.com/jarun/advcpmv/master/install.sh --create-dirs -o ./advcpmv/install.sh && (cd advcpmv && sh install.sh)
-mv ./advcpmv/advcp /usr/bin/cpg
-mv ./advcpmv/advmv /usr/bin/mvg
+
+mv ./advcpmv/advcp /usr/local/bin/cpg
+mv ./advcpmv/advmv /usr/local/bin/mvg
--- files/scripts/install-keymapp.sh
+++ files/scripts/install-keymapp.sh
@@ -2,9 +2,14 @@
 
 set -oue pipefail
 
+mkdir -p /usr/local/bin
+
 wget https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.tar.gz
+
 tar -zxvf keymapp-latest.tar.gz
+
 chmod +x keymapp
-mv keymapp /usr/bin/keymapp
+mv keymapp /usr/local/bin/keymapp
+
 rm keymapp-latest.tar.gz
-rm icon
+rm icon.png
