026b74de7deb667bbdae569e503f4d1a9dbf0296
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2025-05-27 23:12:33 +1000

    fix bin paths

--- files/scripts/install-advcpmv.sh
+++ files/scripts/install-advcpmv.sh
@@ -3,5 +3,5 @@
 set -oue pipefail
 
 curl https://raw.githubusercontent.com/jarun/advcpmv/master/install.sh --create-dirs -o ./advcpmv/install.sh && (cd advcpmv && sh install.sh)
-mv ./advcpmv/advcp /usr/local/bin/cpg
-mv ./advcpmv/advmv /usr/local/bin/mvg
+mv ./advcpmv/advcp /usr/bin/cpg
+mv ./advcpmv/advmv /usr/bin/mvg
