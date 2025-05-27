9283dfcfc2ee49693895d7d181c4fa9bbcc5158e
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2025-05-27 23:55:56 +1000

    let configure run as root

--- files/scripts/install-advcpmv.sh
+++ files/scripts/install-advcpmv.sh
@@ -4,6 +4,9 @@ set -oue pipefail
 
 mkdir -p /usr/local/bin
 
+# configure doesn't like this running as root in build
+export FORCE_UNSAFE_CONFIGURE=1
+
 curl https://raw.githubusercontent.com/jarun/advcpmv/master/install.sh --create-dirs -o ./advcpmv/install.sh && (cd advcpmv && sh install.sh)
 
 mv ./advcpmv/advcp /usr/local/bin/cpg
